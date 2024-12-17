cdd3fe515597ec295cec5e5afc6a672a28f81c78
Greg Sadetsky <lepetitg@gmail.com>
2024-12-17 13:08:41 -0500

    Update server.py

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, lets deploy an even newer newer version!</p>"
+    return "<p>Hello, lets deploy an even newer newer newer version!</p>"
 
 
 app.run(host="0.0.0.0", port=8080)
